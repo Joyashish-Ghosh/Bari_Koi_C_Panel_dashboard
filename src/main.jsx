ba9ab5e09665b2467428f806d07494a8541702c5
Joyashish Ghosh <joyashishghosh@gmail.com>
2025-10-19 17:52:22 +0600

    all code updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import {
   createBrowserRouter,
   RouterProvider,
